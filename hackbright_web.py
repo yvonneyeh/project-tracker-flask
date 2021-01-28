049a3b7e003a4f16cdd0c3557fbf4cf9c02b00e4
Yvonne Yeh <yvonneyeh@me.com>
2021-01-28 02:31:22 +0000

    Create route and form to add new student

--- hackbright_web.py
+++ hackbright_web.py
@@ -11,11 +11,41 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github)
+    return html
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+@app.route("/student-add")
+def add_student():
+    """Show form for entering student info into database"""
+
+    return render_template("student_add.html")
+
+
+@app.route("/student-added", methods=['POST'])
+def add_student_to_db():
+
+    """Add a student."""
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template('confirmation.html', 
+                            github=github)
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
